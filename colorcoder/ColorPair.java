1d1810f9ce5ff90fd80d8e37e51048544295ea9b
ajaysharma1997 <45599664+ajaysharma1997@users.noreply.github.com>
2022-04-11 18:15:47 +0530

    Update ColorPair.java

--- colorcoder/ColorPair.java
+++ colorcoder/ColorPair.java
@@ -1,1 +1,34 @@
-
+package colorcoder;
+public class ColorPair {
+        private MajorColor majorColor;
+        private MinorColor minorColor;
+        
+        public ColorPair(MajorColor major, MinorColor minor)
+        {
+            majorColor = major;
+            minorColor = minor;
+        }
+        public MajorColor getMajor() {
+            return majorColor;
+        }
+        public MinorColor getMinor() {
+            return minorColor;
+        }
+        public static ColorPair GetColorFromPairNumber(int pairNumber) {
+            int zeroBasedPairNumber = pairNumber - 1;
+            MajorColor majorColor = 
+                MajorColor.fromIndex(zeroBasedPairNumber / MinorColor.values().length);
+            MinorColor minorColor =
+                MinorColor.fromIndex(zeroBasedPairNumber % MinorColor.values().length);
+            return new ColorPair(majorColor, minorColor);
+        }
+        public static int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+            return major.getIndex() * MinorColor.values().length + minor.getIndex() + 1;
+        }
+        String ToString() {
+        	String colorPairStr = MajorColor.values()[majorColor.getIndex()].toString();
+            colorPairStr += " ";
+            colorPairStr += MinorColor.values()[minorColor.getIndex()].toString();
+            return colorPairStr;
+        }
+    }
